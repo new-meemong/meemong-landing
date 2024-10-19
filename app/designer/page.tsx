f64a4f88e79f7bae87f83a4974597049d7052281
com8599 <com8599@gmail.com>
2024-10-19 14:26:47 +0900

    feat: route

---
+++ app/designer/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Designer: React.FC = () => {
+  return (
+    <div>
+      <h1>Designer Page</h1>
+      {/* Add your designer page content here */}
+    </div>
+  );
+};
+
+export default Designer;
