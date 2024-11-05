327b6d58b358cc7960e9f22cbb474a318c075c50
digdagto <digdagto@gmail.com>
2024-11-05 10:44:10 +0900

    title

--- src/app/model/layout.tsx
+++ src/app/model/layout.tsx
@@ -3,7 +3,7 @@ import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 
 export const metadata: Metadata = {
-  title: '미몽 - 누구나 헤어모델',
+  title: '미몽 - 누구나 헤어모델, 미용 구인구직까지',
   description:
     '일반인도 손쉽게 헤어모델이 되어 인생머리를 경험하고 미용인과 헤어모델을 위한 구인구직 정보를 한번에 만나보세요. 그리고 창업솔루션까지',
   icons: {
