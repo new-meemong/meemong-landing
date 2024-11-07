6a4fd31073703b50a372310b1e6695f2fdaa3d21
digdagto <digdagto@gmail.com>
2024-11-07 22:46:31 +0900

    title

--- src/app/designer/layout.tsx
+++ src/app/designer/layout.tsx
@@ -3,8 +3,8 @@ import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 
 export const metadata: Metadata = {
-  title: '미몽',
-  description: '헤어디자이너의 모델 구인부터 커리어를 위한 단 하나의 앱, 미몽',
+  title: '누구나 헤어모델, 미용 일자리까지',
+  description: '일반인도 손쉽게 헤어모델이 되어 인생머리를 경험하고 미용인과 헤어모델을 위한 구인구직 정보를 한번에 만나보세요. 그리고 창업솔루션까지',
   icons: {
     icon: '/icons/favicon-purple.svg',
   },
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,9 +6,9 @@ import type { Metadata } from 'next'
 import StyledComponentsRegistry from '../lib/registry'
 
 export const metadata: Metadata = {
-  title: '미몽',
+  title: '누구나 헤어모델, 미용 일자리까지',
   description:
-    '헤어디자이너의 모델 구인부터 모델 지원까지 미용에 관한 모든 것, 미몽',
+    '일반인도 손쉽게 헤어모델이 되어 인생머리를 경험하고 미용인과 헤어모델을 위한 구인구직 정보를 한번에 만나보세요. 그리고 창업솔루션까지',
   icons: {
     icon: '/icons/favicon-purple.svg',
   },
