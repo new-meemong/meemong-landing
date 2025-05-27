a82ab7b10083a5631a8863703c1eacb50242246c
Saehanseul <saehanseul@gmail.com>
2025-05-27 09:10:42 +0900

    feat: metadata title, description 문구 변경

--- src/app/model/layout.tsx
+++ src/app/model/layout.tsx
@@ -4,9 +4,9 @@ import 'slick-carousel/slick/slick-theme.css'
 import { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: '미몽 - 누구나 헤어모델, 미용 구인구직까지',
+  title: '미몽 - 누구나 헤어모델,헤어컨설팅/미용 구인구직까지',
   description:
-    '일반인도 손쉽게 헤어모델이 되어 인생머리를 경험하고 미용인과 헤어모델을 위한 구인구직 정보를 한번에 만나보세요. 그리고 창업솔루션까지',
+    '유명 헤어 디자이너들에게 누구나 헤어모델이 되어 인생머리를 경험하고 집에서 손쉽게 헤어컨설팅도 받아보세요. 그리고 미용구인구직 정보도 한번에 만나보세요.',
   icons: {
     icon: '/icons/favicon-green.svg',
   },
