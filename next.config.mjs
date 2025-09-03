e1c7d4f2b73c576e6ee2d81b8828a51882542bc4
Saehanseul <saehanseul@gmail.com>
2025-09-03 15:58:27 +0900

    feat: app-ads.txt를 제외한 모든 접근 https://www.meemong.com으로 리디렉션

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,21 @@
 const nextConfig = {
   compiler: {
     styledComponents: true
+  },
+  async redirects() {
+    return [
+      {
+        source: '/((?!app-ads\\.txt$|_next|favicon\\.ico|api|images|icons).*)',
+        destination: 'https://www.meemong.com/:path*',
+        permanent: false,
+        has: [
+          {
+            type: 'host',
+            value: '(?!www\\.meemong\\.com).*',
+          }
+        ]
+      }
+    ]
   }
 }
 
